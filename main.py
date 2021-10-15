664ea4a80f0afcfe5af1b2d7cb2d88e2251ef1ac
jonatanr2000 <jonatanrisaasen@gmail.com>
2021-10-15 16:17:43 +0200

    fix stemming

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import random
 import codecs
 import string
+from nltk.probability import FreqDist
+from nltk.stem.porter import PorterStemmer
+
 
 random.seed(123)
 
 f = codecs.open("./2852991.txt", "r", "utf-8")
 
+# Load file
 textfile = f.read().split('\n\n')
+# Remove strings containing 'Gutenberg'
 textfile = [x for x in textfile if 'gutenberg' not in x.lower()]
+# Split strings on space
 textfile = [x.split(' ') for x in textfile]
-#print(textfile)
-remove = string.punctuation+"\n\r\t"
-for str_lst in textfile:
-    for word in str_lst:
-        word = word.translate(remove)
+
+# Create removal-list
+remove = str.maketrans('', '', string.punctuation+"\n\r\t")
+# Remove punctuation and escape characters 
+textfile = [[word.translate(remove) for word in strings] for strings in textfile]
+
+# Inialize stemmer and stem the words
+stemmer = PorterStemmer()
+textfile = [[stemmer.stem(word.lower()) for word in strings] for strings in textfile]
 print(textfile)
-#textfile = [word.translate(None, string.punctuation) for word in [strings for strings in textfile]]
-#print(textfile)
+#freq = FreqDist()
+#print(freq["tax"])
+
