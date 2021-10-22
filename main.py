a246266470e1b5d8dc6d139d1df67713fc95fd60
jonatanr2000 <jonatanrisaasen@gmail.com>
2021-10-22 16:03:59 +0200

    implement task 4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import codecs
 import string
+import copy
 import gensim
 from nltk.probability import FreqDist
 from nltk.stem.porter import PorterStemmer
@@ -24,16 +25,20 @@ def split_on_space(text):
     # Split strings on space
     return [x.split(' ') for x in text]
 
-def remove_punctuation(text):
+def remove_punctuation(text, paragraphs=True):
     # Create removal-list
     remove = str.maketrans('', '', string.punctuation+"\n\r\t")
     # Remove punctuation and escape characters 
-    return [[word.translate(remove) for word in strings] for strings in text]
+    if paragraphs:
+        return [[word.translate(remove) for word in strings] for strings in text]
+    return [word.translate(remove) for word in text]
 
-def stem_text(text):
+def stem_text(text, paragraphs=True):
     # Inialize stemmer and stem the words
     stemmer = PorterStemmer()
-    return [[stemmer.stem(word.lower()) for word in strings] for strings in text]
+    if paragraphs:
+        return [[stemmer.stem(word.lower()) for word in strings] for strings in text]
+    return [stemmer.stem(word.lower()) for word in text]
 
 # Task 2
 
@@ -69,21 +74,43 @@ def bow(stemmed_paragraphs, dictionary):
 
 # Task 3
 
-def build_tfid_model(dictionary):
+def build_tfidf_model(bow):
     # Build tfid model
-    return gensim.models.TfidModel(dictionary)
+    return gensim.models.TfidfModel(bow)
 
-def build_tfid_corpus(tfid_model, bow):
+def build_tfidf_corpus(tfidf_model, bow):
     # Build corpus of tfid
-    return tfid_model[bow]
+    return tfidf_model[bow]
 
-def generate_sim_matrix(tfid_corpus):
+def generate_sim_matrix(tfidf_corpus):
     # Make similarity matrix
-    return gensim.similarities.MatrixSimilarity(tfid_corpus)
+    return gensim.similarities.MatrixSimilarity(tfidf_corpus)
 
-def generate_lsi_model(tfid_corpus, dictionary, topics=100):
+def generate_lsi_model(tfidf_corpus, dictionary, topics=100):
     # Generate LSI model
-    return gensim.models.LsiModel(tfid_corpus, id2word=dictionary, num_topics=topics)
+    return gensim.models.LsiModel(tfidf_corpus, id2word=dictionary, num_topics=topics)
+
+# Task 4
+
+def preprocessing(text):
+    text = text.split(' ')
+    text = remove_punctuation(text, paragraphs=False)
+    text = stem_text(text, paragraphs=False)
+    return text
+
+def show_docs(docs, paragraphs):
+    # This function displays documents on a given format.
+    for doc in docs:
+        p = doc[0]
+        print("\n[Paragraph " + p.__str__() + "]")
+        print(paragraphs[p][:200])
+
+def show_topics(topics, lsi_model):
+    # This function displays topics on a given format.
+    for topic in enumerate(topics):
+        t = topic[1][0]
+        print("\n[Topic " + t.__str__() + "]")
+        print(lsi_model.show_topics()[t])
 
 
 if __name__ == '__main__':
@@ -93,6 +120,7 @@ if __name__ == '__main__':
     paragraphs = get_paragraphs(f)
     # 1.3
     paragraphs = remove_gutenberg(paragraphs)
+    paragraphs_unedited = copy.copy(paragraphs)
     # 1.4
     paragraphs = split_on_space(paragraphs)
     paragraphs = remove_punctuation(paragraphs)
@@ -108,15 +136,42 @@ if __name__ == '__main__':
     bag_of_words, dictionary = bow(stemmed_paragraphs, dictionary)
     
     # 3.1
-    tfid_model = build_tfid_model(dictionary)
+    tfidf_model = build_tfidf_model(bag_of_words)
     # 3.2 
-    tfid_corpus = build_tfid_corpus(tfid_model, bag_of_words)
+    tfidf_corpus = build_tfidf_corpus(tfidf_model, bag_of_words)
     # 3.3
-    sim_matrix = generate_sim_matrix(tfid_corpus)
+    sim_matrix = generate_sim_matrix(tfidf_corpus)
     # 3.4
-    lsi_model = generate_lsi_model(tfid_corpus, dictionary, topics=100)
+    lsi_model = generate_lsi_model(tfidf_corpus, dictionary, topics=100)
     lsi_corpus = lsi_model[bag_of_words]
     lsi_matrix = gensim.similarities.MatrixSimilarity(lsi_corpus)
     # 3.5
-    lsi_model.show_topics() 
+    #print(lsi_model.show_topics())
+
+    # 4.1
+    query = "What is the function of money?".lower()
+    query = preprocessing(query)
+    query = dictionary.doc2bow(query)
+
+    # 4.2
+    index_query_tfidf = tfidf_model[query]
+
+    # 4.3
+    docsim = enumerate(sim_matrix[index_query_tfidf])
+    docs = sorted(docsim, key=lambda kv: -kv[1])[:3]
+
+    # 4.4
+    lsi_query = lsi_model[query]
+    topics = sorted(lsi_query, key=lambda kv: -abs(kv[1]))[:3]
+    show_topics(topics, lsi_model)
+    docsim = enumerate(lsi_matrix[lsi_query])
+    docs = sorted(docsim, key=lambda kv: -kv[1])[:3]
+    show_docs(docs, paragraphs_unedited)
+
+    
+
+    
+
+    
+
 
